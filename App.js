44a1be26f43e243076c0628fa16daa6f32fc9353
LucasCandidoCA <lucasamarale@unipam.edu.br>
2023-12-15 13:05:27 -0300

    Aula 03 - Refatorando

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { StatusBar, SafeAreaView, View } from "react-native";
-import Cesta from "./src/telas/Cesta";
+import Cesta from "./src/telas/cesta";
 import {
   useFonts,
   Montserrat_400Regular,
   Montserrat_700Bold,
 } from "@expo-google-fonts/montserrat";
+import mock from "./src/mocks/cesta";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
   const [fonteCarregada] = useFonts({
@@ -14,13 +16,13 @@ export default function App() {
   });
 
   if (!fonteCarregada) {
-    return <View />;
+    return <AppLoading />;
   }
 
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock} />
     </SafeAreaView>
   );
 }
